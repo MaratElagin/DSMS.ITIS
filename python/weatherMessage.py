44f0ce62e9a8b54b8588403f82f0f3d1f6b92f80
Marat Elagin <elagin@mindbox.cloud>
2023-11-07 15:09:10 +0300

    add wind_speed to weatherMessage

--- python/weatherMessage.py
+++ python/weatherMessage.py
@@ -3,12 +3,12 @@ from pulsar.schema import Record, Double, Integer
 class WeatherMessage(Record):
     id = Integer()
     temperature = Integer()
-    #wind_speed = Double()
+    wind_speed = Double()
     
     def __str__(self):
         functional_vars = {
             'id': self.id,
             'temperature': self.temperature,
-            #'wind_speed': self.wind_speed
+            'wind_speed': self.wind_speed
         }
         return str(functional_vars)
